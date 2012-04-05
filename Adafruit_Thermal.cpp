4cb8322936a3af4b4d1e58b248bbbc88cd4d5315
Paint Your Dragon <paintyourdragon@dslextreme.com>
2012-04-05 10:48:17 -0700

    Manual merge incorporating lazyatom's changes

--- Adafruit_Thermal.cpp
+++ Adafruit_Thermal.cpp
@@ -28,60 +28,39 @@ Adafruit_Thermal::Adafruit_Thermal(int RX_Pin, int TX_Pin) {
 }
 
 void Adafruit_Thermal::begin() {
-#if ARDUINO >= 100
-  _printer = new SoftwareSerial (_RX_Pin, _TX_Pin);
-#else
-  _printer = new NewSoftSerial (_RX_Pin, _TX_Pin);
-#endif
+  begin(150);
+}
+
+// heatTime - 80 is default from page 23 of datasheet. Controls speed of printing and darkness
+void Adafruit_Thermal::begin(int heatTime) {
+  _printer = new SERIAL_IMPL(_RX_Pin, _TX_Pin);
   _printer->begin(19200);
 
-  // The printer can't start receiving data immediately
-  // upon power up -- needs a moment to initialize.  If
-  // Arduino & printer are powered from the same supply,
-  // they're starting simultaneously.  Need to pause for
-  // a moment so the printer is ready for commands.
-  // (A more robust approach might be to wait in a loop
-  // issuing status commands until valid response.)
-  delay(500);
+  reset();
 
-  heatTime = 240; //80 is default from page 23 of datasheet. Controls speed of printing and darkness
-  heatInterval = 2; //2 is default from page 23 of datasheet. Controls speed of printing and darkness
+  heatInterval = 50; //2 is default from page 23 of datasheet. Controls speed of printing and darkness
   printDensity = 15; //Not sure what the defaut is. Testing shows the max helps darken text. From page 23.
   printBreakTime = 15; //Not sure what the defaut is. Testing shows the max helps darken text. From page 23.
 
-#if ARDUINO >= 100
-  _printer->write(27);
-  _printer->write(55);
-  _printer->write(16); //Default 64 dots = 8*('7'+1)
-  _printer->write(heatTime); //Default 80 or 800us
-  _printer->write(heatInterval); //Default 2 or 20us
+  writeBytes(27, 55);
+  writeBytes(7); //Default 64 dots = 8*('7'+1)
+  writeBytes(heatTime); //Default 80 or 800us
+  writeBytes(heatInterval); //Default 2 or 20us
 
   //Modify the print density and timeout
-  _printer->write(18);
-  _printer->write(35);
-  
+  writeBytes(18, 35);
   int printSetting = (printDensity<<4) | printBreakTime;
-  _printer->write(printSetting); //Combination of printDensity and printBreakTime
-#else
-  _printer->print(27, BYTE);
-  _printer->print(55, BYTE);
-  _printer->print(16, BYTE); //Default 64 dots = 8*('7'+1)
-  _printer->print(heatTime, BYTE); //Default 80 or 800us
-  _printer->print(heatInterval, BYTE); //Default 2 or 20us
-
-  //Modify the print density and timeout
-  _printer->print(18, BYTE);
-  _printer->print(35, BYTE);
-  
-  int printSetting = (printDensity<<4) | printBreakTime;
-  _printer->print(printSetting, BYTE); //Combination of printDensity and printBreakTime
-#endif
-  
-  setDefault();
+  writeBytes(printSetting); //Combination of printDensity and printBreakTime
 }
 
+// reset printer
+void Adafruit_Thermal::reset() {
+  writeBytes(27, 64);
+}
+
+// reset formatting
 void Adafruit_Thermal::setDefault(){
-  wake();
+  online();
   justify('L');
   inverseOff();
   doubleHeightOff();
@@ -92,10 +71,13 @@ void Adafruit_Thermal::setDefault(){
   setSize('s');
 }
 
-
 void Adafruit_Thermal::test(){
   println("Hello World!");
   feed(2);
+}
+
+void Adafruit_Thermal::testPage() {
+  writeBytes(18, 84);
 }
 
 // this is the basic function for all printing, the rest is taken care of by the
@@ -107,21 +89,21 @@ size_t Adafruit_Thermal::write(uint8_t c) {
 void Adafruit_Thermal::write(uint8_t c) {
   if (c == 0x13) return;
 #endif
-  if (c != 0xA)  
+  if (c != 0xA)
     linefeedneeded = true;
   else
     linefeedneeded = false;
 
-  //Serial.print(" 0x");
-  //Serial.print(c, HEX);
+  Serial.print(" 0x");
+  Serial.print(c, HEX);
+  Serial.print(" ("); 
 #if ARDUINO >= 100
-  //Serial.print(" ("); Serial.write(c); Serial.println(")");
-  _printer->write(c);
+  Serial.write(c);
 #else
-  //Serial.print(" ("); Serial.print(c, BYTE); Serial.println(")");
-  _printer->print(c);
+  Serial.print(" (");
 #endif
-
+  Serial.println(")");
+  PRINTER_PRINT(c);
   delay(1);
 
 #if ARDUINO >= 100
@@ -140,139 +122,231 @@ void Adafruit_Thermal::printBarcode(char * text, uint8_t type) {
     write(text[i]); //Data
   }
   write(0); //Terminator
-	
+
   delay(3000); //For some reason we can't immediately have line feeds here
   feed(2);
 }
 
+void Adafruit_Thermal::writeBytes(uint8_t a) {
+  PRINTER_PRINT(a);
+}
 
 void Adafruit_Thermal::writeBytes(uint8_t a, uint8_t b) {
-#if ARDUINO >= 100
-  _printer->write(a);
-  _printer->write(b);
-#else
-  _printer->print(a, BYTE);
-  _printer->print(b, BYTE);
-#endif
+  PRINTER_PRINT(a);
+  PRINTER_PRINT(b);
 }
 
 void Adafruit_Thermal::writeBytes(uint8_t a, uint8_t b, uint8_t c) {
-#if ARDUINO >= 100
-  _printer->write(a);
-  _printer->write(b);
-  _printer->write(c);
-#else
-  _printer->print(a, BYTE);
-  _printer->print(b, BYTE);
-  _printer->print(c, BYTE);
-#endif
+  PRINTER_PRINT(a);
+  PRINTER_PRINT(b);
+  PRINTER_PRINT(c);
 }
 
 void Adafruit_Thermal::writeBytes(uint8_t a, uint8_t b, uint8_t c, uint8_t d) {
-#if ARDUINO >= 100
-  _printer->write(a);
-  _printer->write(b);
-  _printer->write(c);
-  _printer->write(d);
-#else
-  _printer->print(a, BYTE);
-  _printer->print(b, BYTE);
-  _printer->print(c, BYTE);
-  _printer->print(d, BYTE);
-#endif
+  PRINTER_PRINT(a);
+  PRINTER_PRINT(b);
+  PRINTER_PRINT(c);
+  PRINTER_PRINT(d);
+}
+
+// === Character commands ===
+
+#define INVERSE_MASK (1 << 1)
+#define UPDOWN_MASK (1 << 2)
+#define BOLD_MASK (1 << 3)
+#define DOUBLE_HEIGHT_MASK (1 << 4)
+#define DOUBLE_WIDTH_MASK (1 << 5)
+#define STRIKE_MASK (1 << 6)
+
+void Adafruit_Thermal::setPrintMode(uint8_t mask) {
+  printMode |= mask;
+  writePrintMode();
+}
+void Adafruit_Thermal::unsetPrintMode(uint8_t mask) {
+  printMode &= ~mask;
+  writePrintMode();
+}
+
+void Adafruit_Thermal::writePrintMode() {
+  writeBytes(27, 33, printMode);
+}
+
+void Adafruit_Thermal::normal() {
+  printMode = 0;
+  writePrintMode();
 }
 
 void Adafruit_Thermal::inverseOn(){
-  writeBytes(29, 'B', 1);
+  setPrintMode(INVERSE_MASK);
 }
 
 void Adafruit_Thermal::inverseOff(){
-  writeBytes(29, 'B', 0, 10);
+  unsetPrintMode(INVERSE_MASK);
+}
+
+void Adafruit_Thermal::upsideDownOn(){
+  setPrintMode(UPDOWN_MASK);
+}
+
+void Adafruit_Thermal::upsideDownOff(){
+  unsetPrintMode(UPDOWN_MASK);
 }
 
 void Adafruit_Thermal::doubleHeightOn(){
-  writeBytes(27, 14);
+  setPrintMode(DOUBLE_HEIGHT_MASK);
 }
 
 void Adafruit_Thermal::doubleHeightOff(){
-  writeBytes(27, 20);
+  unsetPrintMode(DOUBLE_HEIGHT_MASK);
 }
 
+void Adafruit_Thermal::doubleWidthOn(){
+  setPrintMode(DOUBLE_WIDTH_MASK);
+}
+
+void Adafruit_Thermal::doubleWidthOff(){
+  unsetPrintMode(DOUBLE_WIDTH_MASK);
+}
+
+void Adafruit_Thermal::strikeOn(){
+  setPrintMode(STRIKE_MASK);
+}
+
+void Adafruit_Thermal::strikeOff(){
+  unsetPrintMode(STRIKE_MASK);
+}
 
 void Adafruit_Thermal::boldOn(){
-  writeBytes(27, 69, 1);
+  setPrintMode(BOLD_MASK);
 }
 
 void Adafruit_Thermal::boldOff(){
-  writeBytes(27, 69, 0);
-  if (linefeedneeded)
-    feed();
-  linefeedneeded = false;
+  unsetPrintMode(BOLD_MASK);
 }
 
 void Adafruit_Thermal::justify(char value){
   uint8_t pos = 0;
-	
+
   if(value == 'l' || value == 'L') pos = 0;
   if(value == 'c' || value == 'C') pos = 1;
   if(value == 'r' || value == 'R') pos = 2;
-  
+
   writeBytes(0x1B, 0x61, pos);
 }
 
-
+// Feeds by the specified number of lines
 void Adafruit_Thermal::feed(uint8_t x){
+  // The datasheet claims sending bytes 27, 100, <x> will work
+  // but it feeds much much more.
   while (x--)
     write('\n');
 }
 
+// Feeds by the specified number of rows of pixels
+void Adafruit_Thermal::feedRows(uint8_t rows) {
+  writeBytes(27, 74, rows);
+}
+
+void Adafruit_Thermal::flush() {
+  writeBytes(12);
+}
+
 void Adafruit_Thermal::setSize(char value){
   int size = 0;
-  
+
   if(value == 's' || value == 'S') size = 0;
   if(value == 'm' || value == 'M') size = 10;
   if(value == 'l' || value == 'L') size = 25;
-  
+
   writeBytes(29, 33, size, 10);
   // if (linefeedneeded)
   //  println("lfn"); //feed();
   //linefeedneeded = false;
 }
 
-void Adafruit_Thermal::underlineOff() {
-  writeBytes(27, 45, 0, 10);
-}
-void Adafruit_Thermal::underlineOn() {
-  writeBytes(27, 45, 1);
+// Underlines of different weights can be produced:
+// 0 - no underline
+// 1 - normal underline
+// 2 - thick underline
+void Adafruit_Thermal::underlineOn(uint8_t weight) {
+  writeBytes(27, 45, weight);
 }
 
-void Adafruit_Thermal::printBitmap(uint8_t w, uint8_t h,  const uint8_t *bitmap) {
-  writeBytes(18, 42, h, w/8);
-  for (int i=0; i<(w/8) * h; i++) {
-#if ARDUINO >= 100
-    _printer->write(pgm_read_byte(bitmap + i));
-#else
-    _printer->print(pgm_read_byte(bitmap + i), BYTE);
-#endif
+void Adafruit_Thermal::underlineOff() {
+  underlineOn(0);
+}
+
+void Adafruit_Thermal::printBitmap(int w, int h, const uint8_t *bitmap) {
+  if (w > 384) return; // maximum width of the printer
+  for (int rowStart=0; rowStart < h; rowStart += 256) {
+    int chunkHeight = ((h - rowStart) > 255) ? 255 : (h - rowStart);
+    writeBytes(18, 42);
+    writeBytes(chunkHeight, w/8);
+    for (int i=0; i<((w/8)*chunkHeight); i++) {
+      PRINTER_PRINT(pgm_read_byte(bitmap + (rowStart*(w/8)) + i));
+    }
   }
 }
 
+void Adafruit_Thermal::printBitmap(int w, int h, Stream *stream) {
+  if (w > 384) return; // maximum width of the printer
+  for (int rowStart=0; rowStart < h; rowStart += 256) {
+    int chunkHeight = ((h - rowStart) > 255) ? 255 : (h - rowStart);
+    writeBytes(18, 42);
+    writeBytes(chunkHeight, w/8);
+    for (int i=0; i<((w/8)*chunkHeight); i++) {
+      PRINTER_PRINT((uint8_t)stream->read());
+    }
+  }
+};
 
-void Adafruit_Thermal::wake(){
+void Adafruit_Thermal::printBitmap(Stream *stream) {
+  uint8_t tmp;
+  uint16_t width, height;
+
+  tmp = stream->read();
+  width = (stream->read() << 8) + tmp;
+
+  tmp = stream->read();
+  height = (stream->read() << 8) + tmp;
+
+  printBitmap(width, height, stream);
+};
+
+// Take the printer offline. Print commands sent after this will be
+// ignored until `online` is called
+void Adafruit_Thermal::offline(){
+  writeBytes(27, 61, 0);
+}
+
+// Take the printer back online. Subsequent print commands will be
+// obeyed.
+void Adafruit_Thermal::online(){
   writeBytes(27, 61, 1);
 }
 
-void Adafruit_Thermal::sleep(){
-  writeBytes(27, 61, 0);
+// Put the printer into a low-energy state immediately
+void Adafruit_Thermal::sleep() {
+  sleepAfter(0);
+}
+
+// Put the printer into a low-energy state after the given number
+// of seconds
+void Adafruit_Thermal::sleepAfter(uint8_t seconds) {
+  writeBytes(27, 56, seconds);
+}
+
+// Wake the printer from a low-energy state. This command will wait
+// for 50ms (as directed by the datasheet) before allowing further
+// commands to be send.
+void Adafruit_Thermal::wake() {
+  writeBytes(255);
+  delay(50);
 }
 
 ////////////////////// not working?
 void Adafruit_Thermal::tab(){
-#if ARDUINO >= 100
-  _printer->write(9);
-#else
-  _printer->print(9, BYTE);
-#endif
+  PRINTER_PRINT(9);
 }
 void Adafruit_Thermal::setCharSpacing(int spacing) {
   writeBytes(27, 32, 0, 10);
--- Adafruit_Thermal.h
+++ Adafruit_Thermal.h
@@ -25,16 +25,24 @@
 #endif
 
 #define UPC_A 0
-#define UPC_E 1 
+#define UPC_E 1
 #define EAN13 2
-#define EAN8  3 
+#define EAN8  3
 #define CODE39 4
-#define I25   5 
-#define CODEBAR 6 
-#define CODE93 7 
-#define CODE128 8 
+#define I25   5
+#define CODEBAR 6
+#define CODE93 7
+#define CODE128 8
 #define CODE11  9
 #define MSI 10
+
+#if ARDUINO >= 100
+  #define SERIAL_IMPL SoftwareSerial
+  #define PRINTER_PRINT(a) _printer->write(a);
+#else
+  #define SERIAL_IMPL NewSoftSerial
+  #define PRINTER_PRINT(a) _printer->print(a, BYTE);
+#endif
 
 
 class Adafruit_Thermal : public Print {
@@ -42,26 +50,42 @@ class Adafruit_Thermal : public Print {
 
   Adafruit_Thermal(int RX_Pin, int TX_Pin);  // constructor
     void begin();
+    void begin(int heatTime);
+    void reset();
     void setDefault();
     void test();
+    void testPage();
+
 #if ARDUINO >= 100
     size_t write(uint8_t c);
 #else
     void write(uint8_t c);
 #endif
 
+    void normal();
     void inverseOn();
     void inverseOff();
+    void upsideDownOn();
+    void upsideDownOff();
     void doubleHeightOn();
     void doubleHeightOff();
+    void doubleWidthOn();
+    void doubleWidthOff();
     void boldOn();
     void boldOff();
-    void underlineOn();
+    void underlineOn(uint8_t weight=1);
     void underlineOff();
-    
+    void strikeOn();
+    void strikeOff();
+
     void justify(char value);
     void feed(uint8_t x = 1);
+    void feedRows(uint8_t);
+    void flush();
+    void online();
+    void offline();
     void sleep();
+    void sleepAfter(uint8_t seconds);
     void wake();
 
     void setCharSpacing(int spacing);
@@ -71,28 +95,31 @@ class Adafruit_Thermal : public Print {
     void printBarcode(char * text, uint8_t type);
     void setBarcodeHeight(int val);
 
-    void printBitmap(uint8_t w, uint8_t h,  const uint8_t *bitmap);
+    void printBitmap(int w, int h, const uint8_t *bitmap);
+    void printBitmap(int w, int h, Stream *stream);
+    void printBitmap(Stream *stream);
 
     // ??
     void tab();
-    
- private:
-#if ARDUINO >= 100
-    SoftwareSerial * _printer;
-#else
-    NewSoftSerial * _printer;
-#endif
+
+  protected:
+    SERIAL_IMPL * _printer;
     boolean linefeedneeded;
-    
 
     // little helpers to make code easier to read&use
+    void writeBytes(uint8_t a);
     void writeBytes(uint8_t a, uint8_t b);
     void writeBytes(uint8_t a, uint8_t b, uint8_t c);
     void writeBytes(uint8_t a, uint8_t b, uint8_t c, uint8_t d);
 
+    int printMode;
+    void writePrintMode();
+    void setPrintMode(uint8_t mask);
+    void unsetPrintMode(uint8_t mask);
+
     int _RX_Pin;
     int _TX_Pin;
-    
+
     int heatTime;
     int heatInterval;
     char printDensity;
